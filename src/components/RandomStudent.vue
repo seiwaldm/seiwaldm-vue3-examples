e0886b148cac23db8f3619b01e710b49fea13209
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-09-30 13:10:24 +0200

    Update RandomStudent.vue

--- src/components/RandomStudent.vue
+++ src/components/RandomStudent.vue
@@ -5,14 +5,17 @@ const students = ref([
   {
     number: 1,
     name: "Migei",
+    hobbies: ["Tennis", "Skifahren"],
   },
   {
     number: 2,
     name: "Didi",
+    hobbies: ["Gaming"],
   },
   {
     number: 3,
     name: "Gegei",
+    hobbies: ["Tennis", "Gaming"],
   },
   {
     number: 4,
@@ -25,6 +28,7 @@ const students = ref([
   {
     number: 6,
     name: "Portei",
+    hobbies: ["Krafttraining"],
   },
   {
     number: 7,
@@ -37,15 +41,35 @@ const students = ref([
   {
     number: 9,
     name: "Mani",
+    hobbies: ["Fußball"],
   },
 ]);
+
+const klassensprecher = {
+  vorname: "Stefan",
+  nachname: "Portenkirchner",
+  haarfarbe: "braun",
+  augenfarbe: "blau",
+  größe: "187 cm",
+  gewicht: "77 kg",
+};
 </script>
 
 <template>
   <h2>RandomStudent.vue</h2>
-  <ul>
+  <h3>v-for mit Arrays</h3>
+  <ol>
     <li v-for="student in students" :key="student.number">
       {{ student.name }}
+      <ul v-if="student.hobbies">
+        <li v-for="hobby in student.hobbies">
+          {{ hobby }}
+        </li>
+      </ul>
     </li>
+  </ol>
+  <h3>v-for mit einem Objekt</h3>
+  <ul>
+    <li v-for="(value, key) in klassensprecher">{{ key }}: {{ value }}</li>
   </ul>
 </template>
